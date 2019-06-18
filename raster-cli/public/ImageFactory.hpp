908644babbd659d9816deb32e5221063483f27da
Ivan Mollov <ivan.xero.mollov@gmail.com>
2019-06-18 09:04:41 +0300

    Add comments; Format

--- raster-cli/public/ImageFactory.hpp
+++ raster-cli/public/ImageFactory.hpp
@@ -3,6 +3,12 @@
 #include <filesystem>
 #include <fstream>
 
+/**
+ * Reads a type ID from the first whitespace separated string in file.
+ *
+ * @param file_path [in] Path to file.
+ * @returns The format ID of @ref file_path.
+ */
 std::string load_format_id(const std::string& file_path) {
   std::ifstream in(file_path);
   if (!in.is_open()) {
@@ -15,8 +21,18 @@ std::string load_format_id(const std::string& file_path) {
   return format_id;
 }
 
+/**
+ * Creates the appropriate Image sub-type to represent the file.
+ *
+ * @param file_path [in] Path to file.
+ * @returns Image* to the new object or nullptr.
+ */
 Image* create_image(const std::string& file_path) {
   std::string file_format = load_format_id(file_path);
+
+  if (file_format == "P4") {
+    return new PbmMonoBin(file_path);
+  }
 
   return nullptr;
 }
