14a89c089a84d7f6b82a55f3d811afed074f68fd
Ivan Mollov <ivan.xero.mollov@gmail.com>
2019-06-21 02:59:41 +0300

    Enable creation of P4 images

--- raster-cli/public/ImageFactory.hpp
+++ raster-cli/public/ImageFactory.hpp
@@ -26,11 +26,17 @@ static std::string load_format_id(const std::string& file_path) {
 /**
  * Creates the appropriate Image sub-type to represent the file.
  *
+ * @todo Return std::unique_ptr<Image>.
+ *
  * @param file_path [in] Path to file.
  * @returns Image* to the new object or nullptr.
  */
 static Image* create_image(const std::string& file_path) {
   std::string file_format = load_format_id(file_path);
+
+  if (file_format == "P4") {
+    return new P4PbmImage(file_path);
+  }
 
   return nullptr;
 }
