80b1c7f73c48fc7c629f2eb72edf933adcf3e466
Ivan Mollov <ivan.xero.mollov@gmail.com>
2019-06-20 23:38:40 +0300

    Add static specifier to free functions in headers

--- raster-cli/public/ImageFactory.hpp
+++ raster-cli/public/ImageFactory.hpp
@@ -9,7 +9,7 @@
  * @param file_path [in] Path to file.
  * @returns The format ID of @ref file_path.
  */
-std::string load_format_id(const std::string& file_path) {
+static std::string load_format_id(const std::string& file_path) {
   std::ifstream in(file_path);
   if (!in.is_open()) {
     throw std::logic_error("file can not be opened for writing");
@@ -27,7 +27,7 @@ std::string load_format_id(const std::string& file_path) {
  * @param file_path [in] Path to file.
  * @returns Image* to the new object or nullptr.
  */
-Image* create_image(const std::string& file_path) {
+static Image* create_image(const std::string& file_path) {
   std::string file_format = load_format_id(file_path);
 
   if (file_format == "P4") {
