545c44225ce0f64fd42d4935306be6ab8302c26c
Ivan Mollov <ivan.xero.mollov@gmail.com>
2019-06-21 03:46:19 +0300

    Separate declaration from definition PbmImage

--- raster-cli/public/PbmImage.hpp
+++ raster-cli/public/PbmImage.hpp
@@ -8,38 +8,12 @@ class PbmImage : public NetpbmImage {
 public:
   PbmImage(const std::string& file_path) : NetpbmImage(file_path) {}
 
+public:
   enum class Color : bool { BLACK, WHITE };
 
-  Color get_pixel(size_t width, size_t height) const {
-    if (!is_loaded()) {
-      throw std::logic_error("Image is not loaded!");
-    }
+  Color get_pixel(size_t width, size_t height) const;
 
-    size_t index = width / (CHAR_BIT * sizeof(unsigned char));
-    size_t indent = width % (CHAR_BIT * sizeof(unsigned char));
-    unsigned char mask = (1 << indent);
-
-    unsigned char pixel = _pixels.get().at(height).at(index) & mask;
-    return pixel ? Color::BLACK : Color::WHITE;
-  }
-
-  void set_pixel(Color color, unsigned char width, unsigned char height) {
-    if (!is_loaded()) {
-      throw std::logic_error("Image is not loaded!");
-    }
-
-    size_t index = width / (CHAR_BIT * sizeof(unsigned char));
-    size_t indent = width % (CHAR_BIT * sizeof(unsigned char));
-
-    unsigned char& pixels_chunk = _pixels.get().at(height).at(index);
-    unsigned char mask = (1 << indent);
-
-    if (color == Color::BLACK) {
-      pixels_chunk |= mask;
-    } else {
-      pixels_chunk &= ~mask;
-    }
-  }
+  void set_pixel(Color color, unsigned char width, unsigned char height);
 
 private:
   DelayLoad<std::vector<std::vector<unsigned char>>> _pixels;
---
+++ raster-cli/src/PbmImage.cpp
@@ -0,0 +1,37 @@
+#include <PbmImage.hpp>
+
+PbmImage::Color PbmImage::get_pixel(size_t width, size_t height) const {
+  if (!is_loaded()) {
+    throw std::logic_error("Image is not loaded!");
+  }
+
+  const auto& pixels = _pixels.get();
+
+  const size_t index = width / (CHAR_BIT * sizeof(unsigned char));
+  const size_t indent = width % (CHAR_BIT * sizeof(unsigned char));
+
+  const unsigned char mask = (1 << indent);
+
+  unsigned char pixel = pixels.at(height).at(index) & mask;
+  return pixel ? Color::BLACK : Color::WHITE;
+}
+
+void PbmImage::set_pixel(Color color, unsigned char width, unsigned char height) {
+  if (!is_loaded()) {
+    throw std::logic_error("Image is not loaded!");
+  }
+
+  auto& pixels = _pixels.get();
+
+  const size_t index = width / (CHAR_BIT * sizeof(unsigned char));
+  const size_t indent = width % (CHAR_BIT * sizeof(unsigned char));
+
+  unsigned char& pixels_chunk = pixels.at(height).at(index);
+  const unsigned char mask = (1 << indent);
+
+  if (color == Color::BLACK) {
+    pixels_chunk |= mask;
+  } else {
+    pixels_chunk &= ~mask;
+  }
+}
