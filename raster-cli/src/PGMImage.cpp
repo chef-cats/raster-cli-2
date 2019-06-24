307f1ddb1549d57473bdedcda59181ba891d1152
Yoanna Nikolova <nikolowa.yoanna@gmail.com>
2019-06-24 13:35:49 +0300

    Add a definition of the PGM images methods

---
+++ raster-cli/src/PGMImage.cpp
@@ -0,0 +1,39 @@
+#include "..\include\PGMImage.hpp"
+
+
+PGMImage::PGMImage(const std::string& file_name) : NetpbmImage(file_name) {}
+
+/**
+ * Get pixel with coordinates row and column of the image
+ * 
+ * @throw std::out_of_range - if row values is bigger than image's height
+ *                            or column values is bigger than image's width
+ * @throw std::logic_error - if the image is not loaded. 
+ */
+PGM_pixel PGMImage::get_pixel(size_t row, size_t column) const {
+  if (!is_loaded()) {
+    throw std::logic_error("Image is not loaded!");
+  }
+  return _pixels[row][column];
+}
+
+/**
+ * Set new value of the pixel with coordinates row and column
+ *
+ * @throw std::out_of_range - if row values is bigger than image's height
+ *                            or column values is bigger than image's width
+ * @throw std::range_error - if the the pixel value which try to set is bigger than the
+ *                           maximum possible value of the pixels for this image
+ *                           (numbers of grey between black and white).
+ * @throw std::logic_error - if the image is not loaded.
+ */
+void PGMImage::set_pixel(PGM_pixel pixel, size_t row, size_t column) {
+  if (!is_loaded()) {
+    throw std::logic_error("Image is not loaded!");
+  }
+  size_t value = static_cast<size_t>(pixel);
+  if (value > MAX_POSSIBLE_VALUE) {
+    throw std::range_error("Try to set invalid value!");
+  }
+  _pixels[row][column] = pixel;
+}
