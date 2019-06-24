a1432a9c6cb51c82e59c29eee1ed294085c888c2
Ivan Mollov <ivan.xero.mollov@gmail.com>
2019-06-25 00:58:42 +0300

    Implement setters and getters to PBM; is_loaded function

--- raster-cli/public/PbmImage.hpp
+++ raster-cli/public/PbmImage.hpp
@@ -9,12 +9,16 @@ public:
   PbmImage(const std::string& file_path);
 
 public:
-  bool is_loaded() const override;
+  virtual bool is_loaded() const;
 
   enum class Color : bool { BLACK, WHITE };
 
   Color get_pixel(size_t width, size_t height) const;
   void set_pixel(Color color, unsigned char width, unsigned char height);
+
+protected:
+  const std::vector<unsigned char>& get_pixels() const;
+  void set_pixels(const std::vector<unsigned char>& pixels);
 
 private:
   DelayLoad<std::vector<unsigned char>> _pixels;
--- raster-cli/src/PbmImage.cpp
+++ raster-cli/src/PbmImage.cpp
@@ -3,7 +3,7 @@
 PbmImage::PbmImage(const std::string& file_path) : NetpbmImage(file_path) {}
 
 bool PbmImage::is_loaded() const {
-  return NetpbmImage::is_loaded() && _pixels;
+  return is_metadata_loaded() && _pixels;
 }
 
 PbmImage::Color PbmImage::get_pixel(size_t width, size_t height) const {
@@ -40,4 +40,26 @@ void PbmImage::set_pixel(Color color, unsigned char width, unsigned char height)
   } else {
     pixels_chunk &= ~mask;
   }
+}
+
+const std::vector<unsigned char>& PbmImage::get_pixels() const {
+  if (!is_loaded()) {
+    throw std::logic_error("Image is not loaded!");
+  }
+
+  return _pixels.get();
+}
+
+void PbmImage::set_pixels(const std::vector<unsigned char>& pixels) {
+  if (!is_metadata_loaded()) {
+    throw std::logic_error("Image metadata is not loaded!");
+  }
+
+  size_t bits_count = get_width() * get_height();
+  if (bits_count < (pixels.size() - 1) * CHAR_BIT * sizeof(unsigned char)
+      && pixels.size() * CHAR_BIT * sizeof(unsigned char) < bits_count) {
+    throw std::invalid_argument("Size of image doesn't match");
+  }
+
+  _pixels = _pixels;
 }
