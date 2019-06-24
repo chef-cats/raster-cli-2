d941c1a25e4ceaec5326d4bdfe7ca67587668fcf
Ivan Mollov <ivan.xero.mollov@gmail.com>
2019-06-25 02:53:30 +0300

    Fix tests

--- tests/P4PbmImageTests.cpp
+++ tests/P4PbmImageTests.cpp
@@ -8,26 +8,28 @@
 BOOST_AUTO_TEST_SUITE(P4PbmImageTests)
 
 BOOST_AUTO_TEST_CASE(ReadWriteTest) {
-  P4PbmImage image("../../../tests/testData/P4Sample.pbm");
+  const std::string read_file = "../../../tests/testData/P4Sample.pbm";
+  const std::string write_file = "../../../tests/testData/temp/P4SampleTestOutput.pbm";
+
+  P4PbmImage image(read_file);
 
   {
-    std::ifstream in(image._file_path, std::ifstream::binary);
+    std::ifstream in(image._file_path);
     BOOST_REQUIRE(in.is_open());
     image.read(in);
   }
 
   {
-    std::ofstream out("../../../tests/testData/temp/P4SampleTestOutput.pbm");
+    std::ofstream out(write_file);
     BOOST_REQUIRE(out.is_open());
     image.write(out);
   }
 
   {
-    std::ifstream in_original(image._file_path, std::ifstream::binary);
+    std::ifstream in_original(image._file_path);
     BOOST_REQUIRE(in_original.is_open());
 
-    std::ifstream in_copy("../../../tests/testData/temp/P4SampleTestOutput.pbm",
-                          std::ifstream::binary);
+    std::ifstream in_copy(write_file);
     BOOST_REQUIRE(in_copy.is_open());
 
     const std::string content_original((std::istreambuf_iterator<char>(in_original)),
