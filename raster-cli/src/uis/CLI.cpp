f534b6e85186e31897bc29b4a829caae860975e7
Ivan Mollov <ivan.xero.mollov@gmail.com>
2019-06-29 16:36:45 +0300

    Partially implement session info

--- raster-cli/src/uis/CLI.cpp
+++ raster-cli/src/uis/CLI.cpp
@@ -53,6 +53,29 @@ static void args_check(const DelayLoad<std::vector<std::string>>& args,
   }
 }
 
+/// @todo Implement.
+static std::string get_operation_name(OperationId& operation_id) {
+  return "";
+}
+
+static void write_session_info(const Session::Info& session_info, std::ostream& out) {
+  const std::vector<Session::Info> session_info = _current_session->info();
+  std::cout << "You switched to session with ID: " << session_info.get_id() << "!"
+            << std::endl;
+  std::cout << "Name of images in the session:";
+  for (const std::string& image : session_info.get_images()) {
+    std::cout << " " << image;
+  }
+  std::cout << std::endl;
+  std::cout << "Pending transformations:";
+  for (const Session::Info::OperationInfo& operation_info :
+       session_info.get_operations_info()) {
+    std::cout << " " << operation_info.count() << "*"
+              << get_operation_name(operation_info.id());
+  }
+  std::cout << std::endl;
+}
+
 CLI::CLI() : _should_run(true) {}
 
 void CLI::capture_event() {
@@ -80,6 +103,7 @@ void CLI::handle_last_event() {
 
   const auto& args = opt_args.get();
 
+  ///  @todo Save operation handling functions in an unordered_map by ID
   if (cmd == "exit") {
     _should_run = false;
     return;
@@ -105,10 +129,17 @@ void CLI::handle_last_event() {
   } else if (cmd == "save") {
     _current_session->save_all();
   } else if (cmd == "session") {
-    /// @todo Implement.
+    if (args.at(0) == "info") {
+      write_session_info(_current_session->info(), std::cout);
+    } else {
+      throw std::logic_error(Formatter()
+                             << "Unknown session operation '" << args.at(0) << "'");
+    }
   } else if (cmd == "switch") {
     const unsigned long long session_id = std::stoull(args.at(0));
     _current_session = _sessions.begin() + session_id;
+
+    write_session_info(_current_session->info(), std::cout);
   }
 }
 
