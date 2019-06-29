1b50dd298bdfb05ec2561e4cb8cab3e2dc52885a
Ivan Mollov <ivan.xero.mollov@gmail.com>
2019-06-29 16:52:16 +0300

    Add info class to session

--- raster-cli/include/sessions/Session.hpp
+++ raster-cli/include/sessions/Session.hpp
@@ -7,13 +7,12 @@
 
 /**
  * Class to manage a session.
- * 
+ *
  * Provides interface for working with images.
  * @todo Currently this is only a mock-up of the real class. Implement it properly.
  */
 class Session {
 public:
-  Session(unsigned long long id) {}
   Session(unsigned long long id, const std::vector<std::string>& images) {}
 
   void all_to_grayscale() {}
@@ -31,4 +30,26 @@ public:
   void remove_image() {}
 
   void save_all() {}
+
+  class Info {
+  public:
+    unsigned long long get_id() const { return 0; }
+
+    const std::vector<std::string>& get_images() const {
+      return std::vector<std::string>();
+    }
+
+    class OperationInfo {
+    public:
+      size_t get_count() const { return 0; }
+
+      OperationID get_id() const { return OperationID(); }
+    };
+
+    const std::vector<OperationInfo>& get_operations_info() const {
+      return std::vector<OperationInfo>();
+    }
+  };
+
+  Info get_info() const { return Info(); }
 };
