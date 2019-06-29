ca9f871f779226f4a5334cd3dfffa7b612f02406
Ivan Mollov <ivan.xero.mollov@gmail.com>
2019-06-29 16:02:27 +0300

    Refactor handle_last_event

--- raster-cli/src/uis/CLI.cpp
+++ raster-cli/src/uis/CLI.cpp
@@ -45,6 +45,14 @@ static std::vector<std::string> get_args_for(const std::string& cmd, std::istrea
   return args;
 }
 
+static void args_check(const DelayLoad<std::vector<std::string>>& args,
+                       const std::string& cmd) {
+  if (!args || args->size() == 0) {
+    throw std::logic_error(Formatter()
+                           << "Expected argument to '" << cmd << "' operation!");
+  }
+}
+
 CLI::CLI() : _should_run(true) {}
 
 void CLI::capture_event() {
@@ -64,18 +72,19 @@ void CLI::handle_last_event() {
   }
 
   const auto& cmd = _last_event->_cmd;
-  const auto& args = _last_event->_args;
+  const auto& opt_args = _last_event->_args;
+
+  if (needs_args(cmd)) {
+    args_check(opt_args, cmd);
+  }
+
+  const auto& args = opt_args.get();
 
   if (cmd == "exit") {
     _should_run = false;
     return;
   } else if (cmd == "load") {
-    if (args) {
-      _sessions.emplace_back(get_unique_session_id(), args.get());
-    } else {
-      _sessions.emplace_back(get_unique_session_id());
-    }
-
+    _sessions.emplace_back(get_unique_session_id(), args);
     _current_session = std::prev(_sessions.end());
   } else if (cmd == "grayscale") {
     _current_session->all_to_grayscale();
@@ -84,32 +93,21 @@ void CLI::handle_last_event() {
   } else if (cmd == "negative") {
     _current_session->all_to_negative();
   } else if (cmd == "rotate") {
-    if (!args || args->size() == 0) {
-      throw std::logic_error("Expected argument to 'rotate' operation!");
-    }
-    const std::string& direction_token = args->at(0);
+    const std::string& direction_token = args.at(0);
 
     Direction direction = parse_direction(direction_token);
     _current_session->rotate_all(direction);
   } else if (cmd == "undo") {
     /// @todo Implement.
   } else if (cmd == "add") {
-    if (!args || args->size() == 0) {
-      throw std::logic_error("Expected argument to 'add' operation!");
-    }
-    const std::string& image = args->at(0);
-
+    const std::string& image = args.at(0);
     _current_session->add_image(image);
   } else if (cmd == "save") {
     _current_session->save_all();
   } else if (cmd == "session") {
     /// @todo Implement.
   } else if (cmd == "switch") {
-    if (!args || args->size() == 0) {
-      throw std::logic_error("Expected argument to 'switch' operation!");
-    }
-    const unsigned long long session_id = std::stoull(args->at(0));
-
+    const unsigned long long session_id = std::stoull(args.at(0));
     _current_session = _sessions.begin() + session_id;
   }
 }
