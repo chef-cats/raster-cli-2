110d915e3aebed834dec9ffac2f17f6f1011db69
Yoanna Nikolova <nikolowa.yoanna@gmail.com>
2019-06-30 03:47:46 +0300

    Remove impl of already removed write and read - Netpbm

--- raster-cli/src/images/Netpbm.cpp
+++ raster-cli/src/images/Netpbm.cpp
@@ -34,36 +34,6 @@ void Netpbm::load_metadata(std::ifstream& file) {
 }
 
 /**
- * Outputs image's data to a stream.
- *
- * @see metadata_check
- */
-std::ostream& Netpbm::write(std::ostream& out) const {
-    metadata_check();
-
-    return out << get_format_id() << get_width() << get_height();
-}
-
-/**
- * Reads image's data from a stream.
- */
-std::istream& Netpbm::read(std::istream& in) {
-    std::string format_id;
-    in >> format_id;
-    set_format_id(format_id);
-
-    size_t width;
-    in >> width;
-    set_width(width);
-
-    size_t height;
-    in >> height;
-    set_height(height);
-
-    return in;
-}
-
-/**
  * Kind of an assertion. Checks if the image is loaded.
  *
  * @throws std::logic_error - if the image is not loaded.
