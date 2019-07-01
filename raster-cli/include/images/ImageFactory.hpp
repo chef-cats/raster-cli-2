a50a8c453f3883c0cf0b813ebfe9867beb3b7955
Ivan Mollov <ivan.xero.mollov@gmail.com>
2019-07-01 15:02:22 +0300

    Add new image types in the factory

--- raster-cli/include/images/ImageFactory.hpp
+++ raster-cli/include/images/ImageFactory.hpp
@@ -1,6 +1,7 @@
 #pragma once
 
-#include "Image.hpp"
+#include "BinaryPGM.hpp"
+#include "TextPGM.hpp"
 
 #include "utils/HelperFunctions.hpp"
 
@@ -12,6 +13,12 @@
  */
 static std::unique_ptr<Image> create_image(const std::string& file_path) {
     std::string file_format = load_format_id(file_path);
+
+	if (file_format == "P2") {
+        return std::make_unique<TextPGM>(file_path);
+	} else if (file_format == "P5") {
+        return std::make_unique<BinaryPGM>(file_path);
+	}
 
     return nullptr;
 }
