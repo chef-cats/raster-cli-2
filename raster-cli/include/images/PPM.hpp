e07ef9ab2c35f1014f02044c8dbf73091eb01223
Yoanna Nikolova <nikolowa.yoanna@gmail.com>
2019-07-01 12:13:52 +0300

    The PPM declaration.

---
+++ raster-cli/include/images/PPM.hpp
@@ -0,0 +1,32 @@
+#pragma once
+#include <images/Netpbm.hpp>
+#include <vector>
+
+class PPM : public Netpbm {
+  public:
+    class Pixel;
+
+  public:
+    PPM(const std::string& file_name);
+    PPM(const PPM& other) = default;
+    PPM(PPM&& other) = default;
+    PPM& operator=(const PPM& rhs) = default;
+    PPM& operator=(PPM&& rhs) = default;
+    virtual ~PPM() = default;
+
+  public:
+    virtual void apply(const Operation& operation) override;
+    virtual void load() = 0;
+    virtual void save() const = 0;
+
+  public:
+    Pixel get_pixel(size_t row, size_t column) const;
+    void set_pixel(Pixel pixel, size_t row, size_t column);
+   
+  protected:
+    const std::vector<std::vector<Pixel>>& get_pixels() const;
+    void set_pixels(const std::vector<std::vector<Pixel>>& pixels);
+
+  private:
+    DelayLoad<std::vector<std::vector<Pixel>>> _pixels;
+};
