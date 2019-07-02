07669b2ac87a177a9511e245e85df6ef7afc3246
Yoanna Nikolova <nikolowa.yoanna@gmail.com>
2019-07-02 03:12:04 +0300

    Add BinaryPPM tests

--- tests/include/Constants.hpp
+++ tests/include/Constants.hpp
@@ -1,10 +1,12 @@
 #pragma once
 #include <string>
+#include <vector>
 
 const std::string ppm_path = "..\\..\\..\\tests\\TestInput\\PPM\\";
 const std::string ppm_temp_folder = ppm_path + "temp\\";
 
 const std::vector<std::string> PPM_ASCII_FILE_NAMES = {"snail.ascii.ppm"};
+const std::vector<std::string> PPM_BINARY_FILE_NAMES = {"Billes.256.ppm"};
 
 const std::string pgm_path = "..\\..\\..\\tests\\TestInput\\PGM\\";
 const std::string pgm_temp_folder = pgm_path + "temp\\";
